3f2dae232be6620fda397d1d1144a11f4cf08d34
Andrea <zc524@cornell.edu>
2025-07-14 11:37:39 -0400

    organized file into a archive folder

--- slim/plot.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv("tsv_output/dz1_mean_by_psi11_qtl.csv")
-# df.drop(columns=["Unnamed: 0"], inplace=True)
-df.set_index("qtl_count", inplace=True)
-fig = px.imshow(df)
-fig.show()
