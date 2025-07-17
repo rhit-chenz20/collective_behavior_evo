865048070200c3ae0646d83dadca446a2e109caf
Andrea <zc524@cornell.edu>
2025-07-17 01:21:13 -0400

    non0_opt always out-of-memory. run postburnin for base model

--- slim/burnin/base_model/post_burnin_rec.sh
+++ slim/burnin/base_model/post_burnin_rec.sh
@@ -12,9 +12,10 @@ bin/slim5.0 -d psi11=$psi \
             -d "genotype_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/genotype/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             -d "fre_output='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/data/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             -d "ind_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/ind/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
+            -d "mut_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/mut/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             after_burnin_rec.slim &> /workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/log/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.txt &
 
-python split_ind_data.py 
+# python split_ind_data.py 
 
 wait
 
--- slim/burnin/non0_opt/post_burnin.sh
+++ slim/burnin/non0_opt/post_burnin.sh
@@ -12,7 +12,7 @@ bin/slim5.0 -d psi11=$psi \
             -d "genotype_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/genotype/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             -d "fre_output='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/data/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             -d "ind_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/ind/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
-            -d "ind_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/mut/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
+            -d "mut_fn='/workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/mut/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.tsv'" \
             post_burnin_non0.slim &> /workdir/$USER/${SLURM_ARRAY_JOB_ID}-${SLURM_ARRAY_TASK_ID}/log/rec_N_1000_psi_${psi}_pop_${burnin_id}_${simid}.txt &
 
 python split_ind_data.py 
