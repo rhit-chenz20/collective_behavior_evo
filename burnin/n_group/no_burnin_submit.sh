605107d3a9d2fa327128cf0cec4cc05a550e36df
Andrea <zc524@cornell.edu>
2025-08-04 15:45:46 -0400

    changed time limit

--- burnin/n_group/no_burnin_submit.sh
+++ burnin/n_group/no_burnin_submit.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=24:00:00      
 #SBATCH --cpus-per-task=7   
 #SBATCH --mem=14G              
-#SBATCH --partition=short
+#SBATCH --partition=short,long7
 #SBATCH --mail-user=zc524@cornell.edu
 #SBATCH --mail-type=FAIL,END
 
