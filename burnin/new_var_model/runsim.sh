364fb7077c267b22023d743b08a5af3248e27e5b
Andrea <zc524@cornell.edu>
2025-10-07 00:57:30 -0400

    waiting for beta<0.1 to run. most plots ready

--- burnin/new_var_model/runsim.sh
+++ burnin/new_var_model/runsim.sh
@@ -19,7 +19,8 @@ for rep in {1..10}; do
     for n in 2 8 20 50; do
         for sz in 40 80 120; do
             echo "Running simulation with n=${n}"
-            for beta in 0.0 0.05 0.3 0.5 0.7 0.9 1.0; do
+            # for beta in 0.0 0.05 0.3 0.5 0.7 0.9 1.0; do
+            for beta in 0.01 0.03 0.07; do
                 echo "Submitting job for beta=${beta}, n=${n}, sz=${sz}, rep=${rep}."
 
                 bin/slim5.0 -d psi=-100 \
@@ -36,21 +37,21 @@ for rep in {1..10}; do
                     -d "pop_output='../../data/${dir_name}/pop/n_${n}_beta_${beta}_sz_${sz}_reg_var_${rep}.pop'" \
                     var.slim &> ../../data/${dir_name}/log/n_${n}_beta_${beta}_sz_${sz}_reg_var_${rep}.txt &
 
-                # bin/slim5.0 -d psi=-100 \
-                #     -d beta=$beta \
-                #     -d ID="${rep}" \
-                #     -d n=$n \
-                #     -d shift_size=$sz \
-                #     -d "phenotype_fn='../../data/${dir_name}/phenotype/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "genotype_fn='../../data/${dir_name}/genotype/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "fre_output='../../data/${dir_name}/data/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "ind_fn='../../data/${dir_name}/ind/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "mut_fn='../../data/${dir_name}/mut/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "vars_fn='../../data/${dir_name}/vars/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
-                #     -d "pop_output='../../data/${dir_name}/pop/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.pop'" \
-                #     inverse.slim &> ../../data/${dir_name}/log/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.txt &
+                bin/slim5.0 -d psi=-100 \
+                    -d beta=$beta \
+                    -d ID="${rep}" \
+                    -d n=$n \
+                    -d shift_size=$sz \
+                    -d "phenotype_fn='../../data/${dir_name}/phenotype/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "genotype_fn='../../data/${dir_name}/genotype/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "fre_output='../../data/${dir_name}/data/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "ind_fn='../../data/${dir_name}/ind/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "mut_fn='../../data/${dir_name}/mut/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "vars_fn='../../data/${dir_name}/vars/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.tsv'" \
+                    -d "pop_output='../../data/${dir_name}/pop/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.pop'" \
+                    inverse.slim &> ../../data/${dir_name}/log/n_${n}_beta_${beta}_sz_${sz}_reg_inv_${rep}.txt &
             done
         done
+        wait
     done
-    wait
 done
